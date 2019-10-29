29cb3eab495f812f0b8d5678546d48a0edbafe1e
amber <amber20240@gmail.com>
2019-10-29 19:34:34 +0800

    invite card & textContent

---
+++ 2.第14&15章練習/js/all.js
@@ -0,0 +1,8 @@
+document.getElementById("a").textContent="YA!";
+var food =" cookie: ";
+var price = 60;
+document.getElementById("eat").textContent = food + price;
+var name ="儀瑾";
+var polite =",您好";
+var total =name+polite;
+document.getElementById("title").textContent= total;
