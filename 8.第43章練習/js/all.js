75b2851ad01c5f1ea81ef4196f111c3e980c2dbf
amber <amber20240@gmail.com>
2019-11-05 21:18:53 +0800

    practice 比較運算子

---
+++ 8.第43章練習/js/all.js
@@ -0,0 +1,8 @@
+var myMonth = 5;
+var birthdayMonth = 8;
+var checkBirthdayMonth = myMonth == birthdayMonth;
+document.getElementById("birthdayMonth").textContent = checkBirthdayMonth;
+var nowPeople = 1;
+var totalPeople =2;
+var checkNowPeople = nowPeople == totalPeople;
+document.getElementById("checkPeople").textContent = checkNowPeople;
