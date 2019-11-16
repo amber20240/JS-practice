ac4cf0616f373e5cc7e6562cf04bce6e9fbabfee
amber <amber20240@gmail.com>
2019-11-16 11:27:58 +0800

    practice addEventListener

--- 11.第85 ~ 100章練習/all.js
+++ 11.第85 ~ 100章練習/all.js
@@ -3,3 +3,9 @@ var el=document.querySelector(".btn");
 el.onclick=function(e){
     console.log(e)
 };
+//(二)監聽到點擊，顯示出你已經點擊的字樣
+var el2 = document.querySelector(".btn2");
+el2.addEventListener("click",function(){
+    var eltext = document.querySelector("#text");
+    eltext.textContent = "你已經點擊";
+},false);
