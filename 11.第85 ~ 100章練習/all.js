d3f86e0bb5cd56762bf58b7efa7bb2911423b22b
amber <amber20240@gmail.com>
2019-11-16 11:30:48 +0800

    practice onclick / click 的差異

--- 11.第85 ~ 100章練習/all.js
+++ 11.第85 ~ 100章練習/all.js
@@ -9,3 +9,24 @@ el2.addEventListener("click",function(){
     var eltext = document.querySelector("#text");
     eltext.textContent = "你已經點擊";
 },false);
+//(三)綁定事件的語法差異(onclick / click)
+    //1.onclick綁定多個事件，只會觸發最後一個事件
+var el3 = document.querySelector(".btn3");
+el3.onclick=function(){
+  var text3=document.querySelector("#text3");
+  text3.textContent= "1";
+};
+el3.onclick=function(){
+    var text4=document.querySelector("#text4");
+    text4.textContent= "2";
+  };
+  //2.addEventListener("click",function(){},false)綁定多個事件，全部都會觸發
+var el4=document.querySelector(".btn4");
+el4.addEventListener("click",function(){
+   var text5=document.querySelector("#text5");
+   text5.textContent ="1" ;
+},false);
+el4.addEventListener("click",function(){
+    var text6=document.querySelector("#text6");
+    text6.textContent ="2" ;
+ },false);
