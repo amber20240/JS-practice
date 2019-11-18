6679295169121fa540b001acabeb3a9bb242df3d
amber <amber20240@gmail.com>
2019-11-18 17:58:47 +0800

    update to do list

--- 16.第99章練習/all.js
+++ 16.第99章練習/all.js
@@ -1,8 +1,11 @@
-var el =document.querySelector("#ul");
-var el2=document.querySelector("#p");
+var el=document.querySelector("#ul");
+var el2=document.querySelector("#ul2");
+var str2 ="";
 function todolist(e){
-    var str =e.target.nodeName;
+    var str=e.target.nodeName;
+    var str3=e.target.textContent;
     if(str !== "LI"){return};
-    console.log(e.target.textContent)
-}
+    str2 +="<li>"+str3+"</li>";
+    el2.innerHTML=str2;
+};
 el.addEventListener("click",todolist,false);
