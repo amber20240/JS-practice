2bfba46ddfe338d4e1fc6f48851a64708d4aa3ac
amber <amber20240@gmail.com>
2019-11-20 22:30:47 +0800

    JSON.stringify & JSON.parse

--- 17.第101~108章練習/all.js
+++ 17.第101~108章練習/all.js
@@ -1,13 +1,40 @@
+//(一)點選btn1儲存在localStorage，再點選btn2呈現出來
 var el =document.querySelector(".btn1");
-function s(e){
-    var str =document.querySelector("#text").value;
-    localStorage.setItem("name",str);
+function btn1(){
+    var text= document.querySelector("#text").value;
+    localStorage.setItem("name",text);
 };
-el.addEventListener("click",s,false);
+el.addEventListener("click",text,false);
 
 var el2=document.querySelector(".btn2");
-function get(e){
-    var str2 = localStorage.getItem("name");
-    document.querySelector("#em").textContent=str2;
+function btn2(){
+    var text2 =document.querySelector("#em");
+    text2.textContent = localStorage.getItem("name");
 };
-el2.addEventListener("click",get,false);
+el2.addEventListener("click",btn2,false);
+var farms=[{
+    farmer: "cool"
+},{
+    farmer: "star"
+}];
+//(二)點選btn3儲存資料庫farms資料，點選btn4顯示farms資料
+var el3=document.querySelector(".btn3");
+function btn3(){
+    var farmsString=JSON.stringify(farms);
+    localStorage.setItem("farmer",farmsString);
+};
+el3.addEventListener("click",btn3,false);
+
+var el4=document.querySelector(".btn4");
+function btn4(){
+    var farmerString=localStorage.getItem("farmer");
+    var farmerArry=JSON.parse(farmerString);
+    var ul=document.querySelector("#ul");
+    var farmsLen=farms.length;
+    var str="";
+    for(var i=0;i<farmsLen;i++){
+        str +="<li>"+farmerArry[i].farmer+"</li>";
+    };
+    ul.innerHTML=str;
+};
+el4.addEventListener("click",btn4,false);
