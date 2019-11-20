bca0b6121d0cc142c9b2429625c41a7fb7791c90
amber <amber20240@gmail.com>
2019-11-20 22:52:34 +0800

    data-* & array & createElement & edit (一)

--- 17.第101~108章練習/all.js
+++ 17.第101~108章練習/all.js
@@ -1,10 +1,10 @@
 //(一)點選btn1儲存在localStorage，再點選btn2呈現出來
 var el =document.querySelector(".btn1");
-function btn1(){
+function text12(){
     var text= document.querySelector("#text").value;
     localStorage.setItem("name",text);
 };
-el.addEventListener("click",text,false);
+el.addEventListener("click",text12,false);
 
 var el2=document.querySelector(".btn2");
 function btn2(){
@@ -38,3 +38,35 @@ function btn4(){
     ul.innerHTML=str;
 };
 el4.addEventListener("click",btn4,false);
+//(三)data-num/dataset
+var el5 = document.querySelector("#ul2");
+function u2(e){
+    var em2=document.querySelector("#em2");
+    em2.textContent=e.target.dataset.num;
+    var em3=document.querySelector("#em3");
+    em3.textContent=e.target.dataset.dog;
+};
+el5.addEventListener("click",u2,false);
+//(四)把上面的農場印出（for），在印出點到的
+function x(){
+    var str="";
+    var el6=document.querySelector(".ul5");
+    for(var i=0;i<farms.length;i++){
+        str +='<li data-num="'+i+'">'+farms[i].farmer+'</li>';
+    };
+    el6.innerHTML=str;
+};
+x();
+
+var ul6=document.querySelector(".ul5");
+var ul7=document.querySelector(".ul6");
+function checkList(e){
+    var nodeName=e.target.nodeName;
+    var text=e.target.textContent;
+    var num=e.target.dataset.num;
+    if(nodeName !== "LI"){return};
+    var str=document.createElement("li");
+    str.textContent="農場主人是："+text;
+    ul7.appendChild(str);
+};
+ul6.addEventListener("click",checkList,false);
