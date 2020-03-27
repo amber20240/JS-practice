e0d599b932f5e616347a0f299568305edf14420c
Amber <amber20240@gmail.com>
2020-03-27 23:15:15 +0800

    update array to browser not have dataset

--- 13.第95章練習/js/all.js
+++ 13.第95章練習/js/all.js
@@ -1,28 +1,29 @@
-var humbergerNum=document.querySelector("#hamNumId").value;
+var humbergerNum = document.querySelector("#hamNumId").value;
 console.log(humbergerNum);
-var cokeNum=document.querySelector("#cokeNumId").textContent.value;
-var humberPrice =100;
+var cokeNum = document.querySelector("#cokeNumId").textContent.value;
+var humberPrice = 100;
 var cokePrice = 20;
-function count(){
-    var humbergerNum=parseInt(document.querySelector("#hamNumId").value);
-    var cokeNum=parseInt(document.querySelector("#cokeNumId").value);
-    var humberPrice =100;
+function count() {
+    var humbergerNum = parseInt(document.querySelector("#hamNumId").value);
+    var cokeNum = parseInt(document.querySelector("#cokeNumId").value);
+    var humberPrice = 100;
     var cokePrice = 20;
-    var countTotal = humbergerNum*humberPrice + cokeNum*cokePrice;
-    var el2=document.querySelector("#totalId");
-    el2.textContent=countTotal;
+    var countTotal = humbergerNum * humberPrice + cokeNum * cokePrice;
+    var el2 = document.querySelector("#totalId");
+    el2.textContent = countTotal;
+    console.log(humbergerNum);
 };
 var el = document.querySelector("#countId");
-el.addEventListener("click",count,false);
+el.addEventListener("click", count, false);
 
-function t(e){
-    var str =e.target.value;
-    if(str == ""){
+function t(e) {
+    var str = e.target.value;
+    if (str == "") {
         alert("請輸入文字")
     }
 };
 var el3 = document.querySelector("#hamNumId");
-el3.addEventListener("blur",t,false);
+el3.addEventListener("blur", t, false);
 
 var el4 = document.querySelector("#cokeNumId");
-el4.addEventListener("blur",t,false);
+el4.addEventListener("blur", t, false);
